ffe2d75315c0c87af185ae0d5aacdb939769e6d7
Stan___Kudri <53861218+Stan-Kudri@users.noreply.github.com>
2022-01-01 10:58:02 +0300

    Добавьте файлы проекта.

---
+++ JaggedArray/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using JaggedArray;
+
+var arrayGenerator = new ArrayGenerator();
+var array = arrayGenerator.Generate();
+Print(array);
+Console.ReadLine();
+
+void Print(int [][] array)
+{
+    foreach(var innerArray in array)
+        Console.WriteLine("[{0}]", string.Join(", ", innerArray));
+}
